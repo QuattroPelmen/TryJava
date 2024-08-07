39fa3bb5422d0f0814c99d1872f30c987997b30b
QuattroPelmen <aleksey.vernikovsky@gmail.com>
2024-08-07 14:06:23 +0400

    Remove unnecessary comment.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-//Clone the repository, checkout to the branch first-branch and work within it. After the wor is done - just push it. Do not work with master (see readme file)
+
 public class Main {
     public static void main(String[] args) {
         List<String> notSortedList =  new ArrayList<>(Arrays.asList("Please", "sort", "this", "list", "in", "descending", "order")),
