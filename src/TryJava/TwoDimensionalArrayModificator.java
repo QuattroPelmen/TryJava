8ab0d90df23c4fcbc6f630234d838816db5c8400
YURIY <yurivoyt@gmail.com>
2024-08-15 21:59:31 +0200

    Dear Mentor, please check additional 2d array stuff
    Thank you
    Yuriy

---
+++ src/TryJava/TwoDimensionalArrayModificator.java
@@ -0,0 +1,29 @@
+package TryJava;
+
+public class TwoDimensionalArrayModificator {
+    public TwoDimensionalArrayModificator(int[][] inputArray){
+        this.inputArray = inputArray;
+    };
+    private int[][] inputArray;
+    public int[][] arraySort(){
+        int[][] outputArray = new int[inputArray.length][inputArray[0].length];
+        for(int index = 0; index <= inputArray[0].length; index++){
+            if (inputArray[index][0] % 2 == 0){
+                BubbleSort bubbleSort = new BubbleSort(inputArray[index]);
+                bubbleSort.applyBubbleSort();
+                inputArray[index] = bubbleSort.getSortedArray();
+            } else {
+                BubbleSort bubbleSort = new BubbleSort(inputArray[index]);
+                bubbleSort.applyBubbleSortReverse();
+                inputArray[index] = bubbleSort.getSortedArray();
+            }
+        }
+        return outputArray;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = { { 1, 2, 3 }, { 4, 5, 6 } };
+        TwoDimensionalArrayModificator twoDimensionalArrayModificator = new TwoDimensionalArrayModificator(array);
+        twoDimensionalArrayModificator.arraySort();
+    }
+}
