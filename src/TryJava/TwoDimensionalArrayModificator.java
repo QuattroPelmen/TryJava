12d2f2c3f64df6f35c5e2889541a4e2786e789ea
YURIY <yurivoyt@gmail.com>
2024-08-18 20:03:16 +0200

    Dear Mentor, please check modified sorting for different size arrays. Pushed to math fun branch to avoid confusion Thank you Yuriy

--- src/TryJava/TwoDimensionalArrayModificator.java
+++ src/TryJava/TwoDimensionalArrayModificator.java
@@ -1,28 +1,49 @@
 package TryJava;
 
+import java.util.Arrays;
+
 public class TwoDimensionalArrayModificator {
+    private int[][] inputArray;
+    private int[][] outputArray;
     public TwoDimensionalArrayModificator(int[][] inputArray){
         this.inputArray = inputArray;
-    };
-    private int[][] inputArray;
-    public int[][] arraySort(){
-        int[][] outputArray = new int[inputArray.length][inputArray[0].length];
-        for(int index = 0; index <= inputArray[0].length; index++){
+        this.outputArray = Arrays.copyOf(inputArray, inputArray.length);
+    }
+    public void arraySort(){
+        for(int index = 0; index < inputArray.length; index++){
+            System.out.println("input array value " + inputArray[index][0]);
             if (inputArray[index][0] % 2 == 0){
                 BubbleSort bubbleSort = new BubbleSort(inputArray[index]);
                 bubbleSort.applyBubbleSort();
-                inputArray[index] = bubbleSort.getSortedArray();
+                outputArray[index] = bubbleSort.getSortedArray();
             } else {
+                System.out.println("enter else");
                 BubbleSort bubbleSort = new BubbleSort(inputArray[index]);
                 bubbleSort.applyBubbleSortReverse();
-                inputArray[index] = bubbleSort.getSortedArray();
+                outputArray[index] = bubbleSort.getSortedArray();
             }
         }
-        return outputArray;
+    }
+    public void printInputArray(){
+        for(int index = 0; index < inputArray.length; index++){
+            for(int internalIndex = 0; internalIndex < inputArray[index].length; internalIndex++){
+                System.out.print(inputArray[index][internalIndex] + " ");
+            }
+        }
+    }
+    public void printOutputArray(){
+        for(int index = 0; index < outputArray.length; index++){
+            System.out.println("\n" + "Line number: " + index);
+            for(int internalIndex = 0; internalIndex < outputArray[index].length; internalIndex++){
+                System.out.print(outputArray[index][internalIndex] + " ");
+            }
+        }
     }
     public static void main(String[] args) {
-        int[][] array = { { 1, 2, 3 }, { 4, 5, 6 } };
-        TwoDimensionalArrayModificator twoDimensionalArrayModificator = new TwoDimensionalArrayModificator(array);
+        TwoDimensionalArrayModificator twoDimensionalArrayModificator =
+        new TwoDimensionalArrayModificator(new int[][]{{1, 1, 3, 4}, {2, 3, 4, 5, 6, 8}, {4,2,1}});
+        twoDimensionalArrayModificator.printInputArray();
         twoDimensionalArrayModificator.arraySort();
+        twoDimensionalArrayModificator.printOutputArray();
     }
 }
